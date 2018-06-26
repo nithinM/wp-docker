955f5c3d272c83a70ee62fb7f1ee569ce038ea08
Nithin Madhuranga <nithin@axis.lk>
2018-06-26 07:47:13 +0530

    Update README file

--- develop.sh
+++ develop.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 # Set environment variables for dev# Set environment variables for dev
-export CONTAINER_NAME_DATABASE=${CONTAINER_NAME_DATABASE:-fluttersocial-DB}
-export CONTAINER_NAME_APP=${CONTAINER_NAME_APP:-fluttersocial-WP}
-export CONTAINER_NAME_NODE=${CONTAINER_NAME_NODE:-fluttersocial-NODE}
+export CONTAINER_NAME_DATABASE=${CONTAINER_NAME_DATABASE:-WP-Docker-DB}
+export CONTAINER_NAME_APP=${CONTAINER_NAME_APP:-WP-Docker-WP}
+export CONTAINER_NAME_NODE=${CONTAINER_NAME_NODE:-WP-Docker-NODE}
 export DB_PORT=${DB_PORT:-3307}
 export DB_ROOT_PASSWORD=${DB_ROOT_PASSWORD:-password}
-export DB_DATABASE=${DB_DATABASE:-fluttersocialDB}
+export DB_DATABASE=${DB_DATABASE:-WP-Docker-DB}
 export DB_USER=${DB_USER:-wordpress}
 export DB_PASSWORD=${DB_PASSWORD:-password}
 export APP_PORT=${APP_PORT:-80}
